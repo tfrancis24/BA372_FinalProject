eddcf89d3274cd532926c7f8f3c656bac1d1c4cf
Francis, Tim <timothy.s.francis@gmail.com>
2018-05-18 13:00:37 -0700

    Sample code to read from db

--- DatabaseTestApplication/Program.cs
+++ DatabaseTestApplication/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.OleDb;
 
 namespace DatabaseTestApplication
 {
@@ -10,6 +11,38 @@ namespace DatabaseTestApplication
     {
         static void Main(string[] args)
         {
+            //Code for connecting to DB
+            //Need to modify path for our project
+            string connectString = "Provider=Microsoft.ACE.OLEDB.12.0;" + @"Data Source=database._Complete.accdb";
+            OleDbConnection connection = new OleDbConnection(connectString);
+            OleDbCommand cmd = new OleDbCommand();
+            OleDbDataReader reader;
+            cmd.Connection = connection;
+
+            //Open DB
+            //try { connection.Open(); }
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            int counter = 0;
+            //var results = cmd.ExecuteScalar();
+            cmd.CommandText = "SELECT * FROM reviews";
+            connection.Open();
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {//www.csharp-console-example.com
+                counter++;
+                Console.WriteLine(reader[0] + " " + reader[1] + " " + reader[2] + " " + reader[3] + " " + reader[4] + " " + reader[5] + " " + reader[6]);
+            }
+            connection.Close();
+
+            ////Close DB
+            //try { connection.Close(); }
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
         }
     }
 }
